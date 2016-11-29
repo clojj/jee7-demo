c7f2743e30900c3e713ad84f61d100866cb2f86f
clojj <jwin1968@gmail.com>
2016-11-30 00:42:47 +0100

    setup

---
+++ src/main/java/de/fisp/cameldemo/rs/MessageResource.java
@@ -0,0 +1,12 @@
+package de.fisp.cameldemo.rs;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("message")
+public class MessageResource {
+    @GET
+    public String whatever() {
+        return "hey!";
+    }
+}
